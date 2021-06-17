690b5602a3c64d14ceb944ab2ead70fb195890bd
Andrew Bass <48231637+bassandrew35@users.noreply.github.com>
2021-06-17 18:37:21 -0500

    Finishing Example

--- manipulate.py
+++ manipulate.py
@@ -1,15 +1,25 @@
+# Andrew Bass
+# 6/17/21
+
 import json
 
-file = open("data.txt", "r")    # gets the data from file
-data = file.read()
-data = json.loads(data)         # loads data into json format
+# opens dog data file and loads data into variable
+file = open("data.txt", "r")
+text = file.read()
+json_str = json.loads(text)
+
+# finds the data for beagle and changes the ID for the beagle
+for a in json_str:
+    if a["name"] == "Beagle":
+        a["id"] = 99999
+
+# opens output file
 file.close()
 file = open("data2.txt", "w")
-file.write("Dog Breads: \n")
 
-for a in data:                  # gets each name field and writes to another file
-    print(a["name"])
-    file.write(a["name"])
-    file.write("\n")
+# formats data
+json_str = json.dumps(json_str, indent = 2)
 
-file.close()
+# outputs changed data
+print(json_str)
+file.write(json_str)
